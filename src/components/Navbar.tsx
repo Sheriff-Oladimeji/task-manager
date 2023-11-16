893b7dacb09d93049cd31b51cef9ca58a9b17a8b
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-16 16:46:55 +0100

    fet: add toggle mode

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from './ui/toggle'
+
+const Navbar = () => {
+  return (
+      <nav>
+          <ModeToggle/>
+    </nav>
+  )
+}
+
+export default Navbar

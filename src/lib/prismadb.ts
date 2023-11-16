374221ecc2e38788243350c89592e8ebba4f5d62
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-16 18:01:19 +0100

    feat: add prismadb

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,6 +1,35 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "event",
+        level: "error",
+      },
+      {
+        emit: "event",
+        level: "info",
+      },
+      {
+        emit: "event",
+        level: "warn",
+      },
+    ],
+  });
+};
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-export default client;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

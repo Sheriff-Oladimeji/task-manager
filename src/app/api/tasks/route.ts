b9e45512f31b3f8a88172c35efe60270abd6a8f1
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-16 18:48:06 +0100

    feat: add get request api for tasks

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -11,3 +11,12 @@ export const POST = async (request: Request) => {
       return NextResponse.json({message: "Failed to create task", error, status: 500})
   }
 };
+export const GET = async (request: Request) => {
+  try {
+   
+    const tasks = await prisma.task.findMany()
+      return NextResponse.json(tasks,{status: 200})
+  } catch (error) {
+      return NextResponse.json({message: "Failed to fetch tasks", error, status: 500})
+  }
+};

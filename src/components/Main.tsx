081f772095ec9278647ad65d1fd146336f6ef222
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-21 11:36:35 +0100

    feat: add metadata
    add: react icons library

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='sm:col-span-2 md:col-span-4 sm:row-span-4 overflow-y-auto'>Main</div>
+    <div className='sm:col-span-2 md:col-span-4 sm:row-span-5 overflow-y-auto'>Main</div>
   )
 }
 
